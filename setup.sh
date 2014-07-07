9bcbec8a2b51af99823f1264f35e11efccab5389
alber sánchez <albhasan@gmail.com>
2014-07-07 18:09:10 +0200

    Update setup.sh
    Start a container and ssh to it must be controlled by users

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ docker stop scidb1
 docker ps -a  | grep -v "Up"  | grep -v "STATUS" | awk '{ print $1 }' | xargs docker rm
 docker images  | grep -v "TAG"  | grep "<none>" | awk '{print $3}' | xargs docker rmi
 docker build --rm=true --tag="scidb_img" .
-docker run -d -P --name="scidb1" -p 49901:49901 -p 49903:49903 -p 49904:49904 --expose=49902 --expose=49910 -v /var/bliss/scidb/test:/home/scidb/data scidb_img 
-ssh -p 49901 root@localhost
+#docker run -d -P --name="scidb1" -p 49901:49901 -p 49903:49903 -p 49904:49904 --expose=49902 --expose=49910 -v /var/bliss/scidb/test:/home/scidb/data scidb_img 
+#ssh -p 49901 root@localhost
