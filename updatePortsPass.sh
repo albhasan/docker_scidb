3d283f5778d051acdd68410f2517d103635a5292
albhasan <albhasan@gmail.com>
2014-07-11 12:17:25 +0200

    Added update for scidb configuration name

--- updatePortsPass.sh
+++ updatePortsPass.sh
@@ -3,6 +3,7 @@
 #REPLACE DEFAULT PORTS AND PASSWORDS
 ##############################################################
 find ./* -exec sed -i 's/xxxx.xxxx.xxxx/yyyy.yyyy.yyyy/g' {} \;
+find ./* -exec sed -i 's/scidb_docker/scidb_docker_bigdata/g' {} \;
 find ./* -exec sed -i 's/49901/49951/g' {} \;
 find ./* -exec sed -i 's/49902/49952/g' {} \;
 find ./* -exec sed -i 's/49903/49953/g' {} \;
