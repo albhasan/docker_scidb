dec343743e0e00ad61a52ace29b3d9ba3bdab49e
albhasan <albhasan@gmail.com>
2014-07-11 11:45:03 +0200

    Added script to update passwords and ports on the files

---
+++ updatePortsPass.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+##############################################################
+#REPLACE DEFAULT PORTS AND PASSWORDS
+##############################################################
+find ./* -exec sed -i 's/xxxx.xxxx.xxxx/yyyy.yyyy.yyyy/g' {} \;
+find ./* -exec sed -i 's/49901/49951/g' {} \;
+find ./* -exec sed -i 's/49902/49952/g' {} \;
+find ./* -exec sed -i 's/49903/49953/g' {} \;
+find ./* -exec sed -i 's/49904/49954/g' {} \;
+find ./* -exec sed -i 's/49910/49960/g' {} \;
+find ./* -exec sed -i 's/scidb1/scidb_modis1/g' {} \;
+find ./* -exec sed -i 's/scidb_img/scidb_modis_img/g' {} \;
