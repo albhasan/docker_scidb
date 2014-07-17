2e44599e45e3f1003e199f452877ed99ba9ddd52
albhasan <albhasan@gmail.com>
2014-07-17 10:52:59 +0200

    Comments added

--- updatePortsPass.sh
+++ updatePortsPass.sh
@@ -2,12 +2,21 @@
 ##############################################################
 #REPLACE DEFAULT PORTS AND PASSWORDS
 ##############################################################
-find ./* -exec sed -i 's/xxxx.xxxx.xxxx/yyyy.yyyy.yyyy/g' {} \;
-find ./* -exec sed -i 's/scidb_docker/scidb_docker_bigdata/g' {} \;
-find ./* -exec sed -i 's/49901/49951/g' {} \;
-find ./* -exec sed -i 's/49902/49952/g' {} \;
-find ./* -exec sed -i 's/49903/49953/g' {} \;
-find ./* -exec sed -i 's/49904/49954/g' {} \;
-find ./* -exec sed -i 's/49910/49960/g' {} \;
-find ./* -exec sed -i 's/scidb1/scidb_modis1/g' {} \;
+
+# Docker image name
 find ./* -exec sed -i 's/scidb_img/scidb_modis_img/g' {} \;
+# Container name
+find ./* -exec sed -i 's/scidb1/scidb_modis1/g' {} \;
+# Container's passwords for root, postgres & scidb users
+find ./* -exec sed -i 's/xxxx.xxxx.xxxx/yyyy.yyyy.yyyy/g' {} \;
+# Container's SciDB configuration - see config.ini
+find ./* -exec sed -i 's/scidb_docker/scidb_docker_bigdata/g' {} \;
+# Container's SSH port
+find ./* -exec sed -i 's/49901/49951/g' {} \;
+# Container's passwords for root, postgres & scidb users
+# Container's POSTGRESQL port
+find ./* -exec sed -i 's/49902/49952/g' {} \;
+# Container's SHIM secured port - see conf
+find ./* -exec sed -i 's/49904/49954/g' {} \;
+# Container's SCIDB port
+find ./* -exec sed -i 's/49910/49960/g' {} \;
