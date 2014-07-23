a97e876a791ae2df564b6cb59c593df77cdc96bb
albhasan <albhasan@gmail.com>
2014-07-23 14:56:03 +0200

    SciDB compiling

--- dev/containerSetup.sh
+++ dev/containerSetup.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
+chmod u+s /usr/bin/sudo
+dpkg --list | grep scidb | awk '{print $2}' | xargs sudo dpkg --purge
+#adduser scidb sudo
+#adduser postgres sudo
 sudo su scidb
 cd ~
 export LC_ALL="en_US.UTF-8"
 export SCIDB_VER=14.3
 export SCIDB_INSTALL_PATH=/home/scidb/dev_dir/scidbtrunk/stage/install
 export PATH=$SCIDB_INSTALL_PATH/bin:$PATH
-dpkg --list | grep scidb | awk '{print $2}' | xargs sudo dpkg --purge
 yes | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
-echo xxxx.xxxx.xxxx | /home/scidb/dev_dir/scidbtrunk/deployment/deploy.sh access root "" "" localhost 
-echo xxxx.xxxx.xxxx | /home/scidb/dev_dir/scidbtrunk/deployment/deploy.sh access scidb "" "" localhost 
+echo xxxx.xxxx.xxxx | /home/scidb/dev_dir/scidbtrunk/deployment/deploy.sh access root "" "" localhost
+echo xxxx.xxxx.xxxx | /home/scidb/dev_dir/scidbtrunk/deployment/deploy.sh access scidb "" "" localhost
 /home/scidb/dev_dir/scidbtrunk/deployment/./deploy.sh prepare_toolchain localhost
 /home/scidb/dev_dir/scidbtrunk/deployment/deploy.sh prepare_postgresql postgres postgres 255.255.0.0/16 localhost
 exit
 usermod -G scidb -a postgres
 chmod g+rx /home/scidb/dev_dir
 sudo su scidb
-cd ~ 
+cd ~
 yes | /home/scidb/dev_dir/scidbtrunk/./run.py setup
 /home/scidb/dev_dir/scidbtrunk/./run.py make -j4
+export LC_ALL="en_US.UTF-8"
 yes | /home/scidb/dev_dir/scidbtrunk/./run.py install
 /home/scidb/dev_dir/scidbtrunk/./run.py start
 /home/scidb/dev_dir/scidbtrunk/./run.py stop
--- dev/setup.sh
+++ dev/setup.sh
@@ -4,5 +4,5 @@ docker rm scidb_dev1
 docker rmi scidb_dev_img
 docker build --rm=true --tag="scidb_dev_img" .
 
-#docker run -d -P --name="scidb_dev1" -p 49901:22  --expose=22 --expose=1239 --expose=5432 scidb_dev_img
+#docker run -d --name="scidb_dev1" -p 49901:22  --expose=22 --expose=1239 --expose=5432 scidb_dev_img
 #ssh -p 49901 root@localhost
