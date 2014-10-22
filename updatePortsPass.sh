fd9be4dba0b87811156654845fd6203e25531974
albhasan <albhasan@gmail.com>
2014-10-22 12:05:35 +0200

    updatePortsPass.sh updated

--- updatePortsPass.sh
+++ updatePortsPass.sh
@@ -9,14 +9,12 @@ find ./* -exec sed -i 's/scidb_img/scidb_modis_img/g' {} \;
 find ./* -exec sed -i 's/scidb1/scidb_modis1/g' {} \;
 # Container's passwords for root, postgres & scidb users
 find ./* -exec sed -i 's/xxxx.xxxx.xxxx/yyyy.yyyy.yyyy/g' {} \;
-# Container's SciDB configuration - see config.ini
-find ./* -exec sed -i 's/scidb_docker/scidb_docker_bigdata/g' {} \;
 # Container's SSH port
-find ./* -exec sed -i 's/49901/49951/g' {} \;
+find ./* -exec sed -i 's/49901/49971/g' {} \;
 # Container's passwords for root, postgres & scidb users
 # Container's POSTGRESQL port
-find ./* -exec sed -i 's/49902/49952/g' {} \;
+find ./* -exec sed -i 's/49902/49972/g' {} \;
 # Container's SHIM secured port - see conf
-find ./* -exec sed -i 's/49904/49954/g' {} \;
+find ./* -exec sed -i 's/49903/49973/g' {} \;
 # Container's SCIDB port
-find ./* -exec sed -i 's/49910/49960/g' {} \;
+find ./* -exec sed -i 's/49904/49974/g' {} \;
